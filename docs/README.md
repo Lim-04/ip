b7373bfd02053f2a3a0d52c841936348fb2e3ba4
Lim Jun Hong Jeff <limjunhongjeff21@gmail.com>
2026-08-21 13:55:31 +0800

    Rename chatbot from Duke to XiaoZhi

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# XiaoZhi User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/XiaoZhi.java
@@ -0,0 +1,10 @@
+public class XiaoZhi {
+    public static void main(String[] args) {
+        String banner = "__  ___            ______     _ \n"
+                + "\\ \\/ (_) __ _  ___|__  / |__ (_)\n"
+                + " \\  /| |/ _` |/ _ \\ / /| '_ \\| |\n"
+                + " /  \\| | (_| | (_) / /_| | | | |\n"
+                + "/_/\\_\\_|\\__,_|\\___/____|_| |_|_|\n";
+        System.out.println(banner);
+    }
+}
