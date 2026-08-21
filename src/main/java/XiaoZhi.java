4dc2b4297bdc8209ad9efc4ba5f58496366540ff
Lim Jun Hong Jeff <limjunhongjeff21@gmail.com>
2026-08-21 14:07:45 +0800

    Add greet and exit messages

--- src/main/java/XiaoZhi.java
+++ src/main/java/XiaoZhi.java
@@ -1,10 +1,26 @@
 public class XiaoZhi {
     public static void main(String[] args) {
-        String banner = "__  ___            ______     _ \n"
-                + "\\ \\/ (_) __ _  ___|__  / |__ (_)\n"
-                + " \\  /| |/ _` |/ _ \\ / /| '_ \\| |\n"
-                + " /  \\| | (_| | (_) / /_| | | | |\n"
-                + "/_/\\_\\_|\\__,_|\\___/____|_| |_|_|\n";
+      printBanner();
+      printGreet();
+      printFarewell();
+    }
+
+    private static void printBanner() {
+        String banner = """
+            __  ___            ______     _ 
+            \\ \\/ (_) __ _  ___|__  / |__ (_)
+             \\  /| |/ _` |/ _ \\ / /| '_ \\| |
+             /  \\| | (_| | (_) / /_| | | | |
+            /_/\\_\\_|\\__,_|\\___/____|_| |_|_|
+            """;
         System.out.println(banner);
+    }
+
+    private static void printGreet() {
+        System.out.println("Hi! I'm XiaoZhi.\nWhat's the task for today?");
+    }
+
+    private static void printFarewell() {
+        System.out.println("Bye, See you soon!");
     }
 }
